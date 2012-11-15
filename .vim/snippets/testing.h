4b05165d480c287fac8cb60e9f79248153cfd3ae
Sergei Nikulov <sergey.nikulov@gmail.com>
2012-11-15 12:38:41 +0400

    fixed once on windows

---
+++ .vim/snippets/testing.h
@@ -0,0 +1,49 @@
+#ifndef TESTING_H_INCLUDED__
+
+#define TESTING_H_INCLUDED__
+
+
+
+#endif /* end of include guard: TESTING_H_INCLUDED__ */
+#ifndef ZZ
+
+#define ZZ
+
+
+
+#endif /* end of include guard: ZZ */
+#ifndef 0strftime("%Y%M%D%T").
+
+#define 0strftime("%Y%M%D%T").
+
+
+
+#endif /* end of include guard: 0strftime("%Y%M%D%T"). */
+#ifndef 0strftime("%Y%M%D%T")0
+
+#define 0strftime("%Y%M%D%T")0
+
+
+
+#endif /* end of include guard: 0strftime("%Y%M%D%T")0 */
+#ifndef ZZ
+
+#define ZZ
+
+
+
+#endif /* end of include guard: ZZ */
+#ifndef TESTING..'H_INCLUDED__'
+
+#define TESTING..'H_INCLUDED__'
+
+
+
+#endif /* end of include guard: TESTING..'H_INCLUDED__' */
+#ifndef 0strftime("%Y%M%D%T")0
+
+#define 0strftime("%Y%M%D%T")0
+
+
+
+#endif /* end of include guard: 0strftime("%Y%M%D%T")0 */
